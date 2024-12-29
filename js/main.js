fae87cc9916aa4a5a8462874c96e22a0bd9c4695
7Eltantawy <50374022+7Eltantawy@users.noreply.github.com>
2024-12-29 19:19:56 +0200

    update card widget to only show googlePlay related links when googlePlayURL is exist

--- js/main.js
+++ js/main.js
@@ -9,17 +9,27 @@ function createCard(app) {
           app.githubIdentifier
         )}" alt="GitHub Version" class="badge" />
         
-        <img src="${Links.googlePlayBadge(
+        ${
           app.packageId
-        )}" alt="Google Play Downloads" class="badge" />
-      </div>
+            ? `<img
+              src="${Links.googlePlayBadge(app.packageId)}"
+              alt="Google Play Downloads"
+              class="badge"
+            />`
+            : ""
+        }
+          </div>
       
       <p>${app.shortDesc}</p>
       
       <div class="buttons">
-        <a href="${Links.googlePlayURL(
+        ${
           app.packageId
-        )}" target="_blank" class="fab fa-google-play"></a>
+            ? `<a href="${Links.googlePlayURL(
+                app.packageId
+              )}" target="_blank" class="fab fa-google-play"></a>`
+            : ""
+        }
         <a href="${Links.windowsURL(
           app.githubIdentifier
         )}" target="_blank" class="fab fa-windows"></a>
